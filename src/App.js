fafdbe6da60138303b892e8b5dd9e7fb15ecedd6
vladmotpan <vladmotpan@gmail.com>
2019-09-05 00:41:28 +0300

    Config editor toolbar & image upload

--- src/App.js
+++ src/App.js
@@ -4,36 +4,40 @@ import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 
 const editorConfiguration = {
-    plugins: [Essentials, Bold, Italic, Paragraph],
-    toolbar: ['bold', 'italic']
+    plugins: [SimpleUploadAdapter, Essentials, Heading, Bold, Italic, Image, ImageUpload],
+    toolbar: ['heading', '|', 'undo', 'redo', 'bold', 'italic', 'imageUpload'],
+    simpleUpload: {
+        uploadUrl: '',
+        headers: {}
+    }
 };
 
 export default () => (
-    <div className="App">
-        <h2>Using CKEditor 5 from source in React</h2>
-        <CKEditor
-            editor={ClassicEditor}
-            config={editorConfiguration}
-            data="<p>Hello from CKEditor 5!</p>"
-            onInit={editor => {
-                // You can store the "editor" and use when it is needed.
-                console.log('Editor is ready to use!', editor);
-            }}
-            onChange={(event, editor) => {
-                const data = editor.getData();
-                console.log({event, editor, data});
-            }}
-            onBlur={(event, editor) => {
-                console.log('Blur.', editor);
-            }}
-            onFocus={(event, editor) => {
-                console.log('Focus.', editor);
-            }}
-        />
-    </div>
+    <CKEditor
+        editor={ClassicEditor}
+        config={editorConfiguration}
+        data="<p>Hello from CKEditor 5!</p>"
+        onInit={editor => {
+            // You can store the "editor" and use when it is needed.
+            console.log('Editor is ready to use!', editor);
+        }}
+        onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({event, editor, data});
+        }}
+        onBlur={(event, editor) => {
+            console.log('Blur.', editor);
+        }}
+        onFocus={(event, editor) => {
+            console.log('Focus.', editor);
+        }}
+    />
 );
